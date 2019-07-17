fb0bb2ed76ec1ee8dae9f7991bc8307ac6df0477
Desiree Morris <desiquinncompany@gmail.com>
2019-07-17 18:33:17 -0400

    pulled data from nasa API

--- src/App.js
+++ src/App.js
@@ -1,7 +1,23 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
+import axios from "axios";
 import "./App.css";
 
+
 function App() {
+
+  const [photoData ,setPhotoData ] = useState([]);
+
+    // Pulling the data from the API
+    useEffect( () => {
+        axios.get('https://api.nasa.gov/planetary/apod?api_key=jZVrib76XnPayK61vx8TCtZIkKqgKHab4SrgI9kf')
+            .then(response => {
+              const photoOfDay = response.data;
+              setPhotoData(photoOfDay)
+            })
+            .catch(error => console.log("Oh No What Happened!", error))
+    }, []);
+    
+
   return (
     <div className="App">
       <p>
